8cff35dc73a05c55bff635a049c5e7fb6ae06528
Edson A. <sigowtec.sigowtec@gmail.com>
2026-08-05 23:06:12 -0300

    feat: trata espaços em branco genero perfil interativo usuário

--- cinematch.js
+++ cinematch.js
@@ -3,7 +3,7 @@ const prompt = require("prompt-sync")();
 const usuario = {
                     nome: prompt("Informe seu Nome: "), 
                     idade: Number(prompt("Informe sua idade: ")), 
-                    generosFavoritos: prompt("Informe os gêneros que você mais gosta.(Separe por vírgula, ex: Ação, Comédia, Terror): ")  
+                    generosFavoritos: prompt("Informe os gêneros que você mais gosta.(Separe por vírgula, ex: Ação, Comédia, Terror): ").split(',').map((g) => g.trim())  
 };
 
 console.log(usuario);
