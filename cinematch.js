43de4e82e8743f80e514d052e4aa280868256f3a
Edson A. <sigowtec.sigowtec@gmail.com>
2026-08-06 00:52:47 -0300

    feat(validacao): implementa validação de obrigatoriedade dados perfil

--- cinematch.js
+++ cinematch.js
@@ -1,9 +1,16 @@
 const prompt = require("prompt-sync")();
 
-const usuario = {
-                    nome: prompt("Informe seu Nome: "), 
-                    idade: Number(prompt("Informe sua idade: ")), 
-                    generosFavoritos: prompt("Informe os gêneros que você mais gosta.(Separe por vírgula, ex: Ação, Comédia, Terror): ").split(',').map((g) => g.trim())  
-};
+let usuario;
+let campoObrigatorio = ''; 
+do {
+    usuario = {
+        nome: prompt("Informe seu Nome(*): "), 
+        idade: Number(prompt("Informe sua idade: ")), 
+        generosFavoritos: prompt("Informe os gêneros que você mais gosta.(Separe por vírgula, ex: Ação, Comédia, Terror)(*): ").split(',').map((g) => g.trim())  
+    };
+    // IF TERNÁRIO
+    //console.log(typeof usuario.nome.trim());
+    campoObrigatorio = ((usuario.nome.trim() === '' || usuario.generosFavoritos.every(g => g === '')) ?  console.log(`Obrigatório preencher seu nome e ao menos um gênero que gosta!`) : 'OK');   
+} while( campoObrigatorio != 'OK');
 
 console.log(usuario);
