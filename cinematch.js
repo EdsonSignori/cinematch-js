8fd733e2c64cfcf34c9bbb6c8fde5e80c9b15eb4
Edson A. <sigowtec.sigowtec@gmail.com>
2026-08-07 22:54:41 -0300

    refactor(cria classes): simplificação por classes e herança para alimentar catálogo

--- cinematch.js
+++ cinematch.js
@@ -1,130 +1,70 @@
 const prompt = require("prompt-sync")();
 
-let usuario;
-let campoObrigatorio = ''; 
-do {
-    usuario = {
-        nome: prompt("Informe seu Nome(*): "), 
-        idade: Number(prompt("Informe sua idade: ")), 
-        generosFavoritos: prompt("Informe os gêneros que você mais gosta.(Separe por vírgula, ex: Ação, Comédia, Terror)(*): ").split(',').map((g) => g.trim())  
-    };
-    // IF TERNÁRIO COM MÉTODO DE ARRAY EVERY PARA VALIDAR CAMPOS OBRIGATÓRIOS
-    //console.log(typeof usuario.nome.trim());
-    campoObrigatorio = ((usuario.nome.trim() === '' || usuario.generosFavoritos.every(g => g === '')) ?  console.log(`Obrigatório preencher seu nome e ao menos um gênero que gosta!`) : 'OK');   
-} while( campoObrigatorio != 'OK');
+// RF01 - PERFIL INTERATIVO PESSOA USUARIA
+    let usuario;
+    let campoObrigatorio = ''; 
+    do {
+        usuario = {
+            nome: prompt("Informe seu Nome(*): "), 
+            idade: Number(prompt("Informe sua idade: ")), 
+            generosFavoritos: prompt("Informe os gêneros que você mais gosta.(Separe por vírgula, ex: Ação, Comédia, Terror)(*): ").split(',').map((g) => g.trim())  
+        };
+        // IF TERNÁRIO COM MÉTODO DE ARRAY EVERY PARA VALIDAR CAMPOS OBRIGATÓRIOS
+        //console.log(typeof usuario.nome.trim());
+        campoObrigatorio = ((usuario.nome.trim() === '' || usuario.generosFavoritos.every(g => g === '')) ?  console.log(`Obrigatório preencher seu nome e ao menos um gênero que gosta!`) : 'OK');   
+    } while( campoObrigatorio != 'OK');
+    // console.log(usuario);
 
-console.log(usuario);
-
-// ARRAY CATALOGO DE CONTEUDOS
-
-const catalogo = [
-    { 
-        id: 1,
-        titulo: "Horizonte Sem Fim",
-        tipo: "Filme",
-        generos: ["Ficção Científica","Drama", "Aventura"],
-        duracaoMinutos: 148,    
-        temporadas: 0
-    },
-    { 
-        id: 2,
-        titulo: "Cidade das Sombras",
-        tipo: "Série",
-        generos: ["Suspense", "Crime", "Drama"],
-        duracaoMinutos: 75,    
-        temporadas: 6
-    },
-    { 
-        id: 3,
-        titulo: "Operação Tempestade",
-        tipo: "Filme",
-        generos: ["Ação","Guerra"],
-        duracaoMinutos: 129,    
-        temporadas: 0
-    },
-    { 
-        id: 4,
-        titulo: "Código Eclipse",
-        tipo: "Série",
-        generos: ["Ficção Científica","Mistério"],
-        duracaoMinutos: 56,    
-        temporadas: 4
-    },
-    { 
-        id: 5,
-        titulo: "Missão Final",
-        tipo: "Filme",
-        generos: ["Ação", "Aventura"],
-        duracaoMinutos: 120,    
-        temporadas: 0
-    },
-    { 
-        id: 6,
-        titulo: "Ecos do Amanhã",
-        tipo: "Série",
-        generos: ["Drama", "Ficção Científica", "Mistério"],
-        duracaoMinutos: 215,    
-        temporadas: 15
-    },
-    { 
-        id: 7,
-        titulo: "O Reino Encantado",
-        tipo: "Filme",
-        generos: ["Animação", "Família", "Fantasia"],
-        duracaoMinutos: 118,    
-        temporadas: 0
-    },
-    { 
-        id: 8,
-        titulo: "Confusão no Prédio",
-        tipo: "Série",
-        generos: ["Comédia"],
-        duracaoMinutos: 145,    
-        temporadas: 9
-    },
-    { 
-        id: 10,
-        titulo: "A Mansão da Neblina",
-        tipo: "Filme",
-        generos: ["Terror", "Suspense"],
-        duracaoMinutos: 145,    
-        temporadas: 0
-    },
-    { 
-        id: 11,
-        titulo: "Corações ao Entardecer",
-        tipo: "Filme",
-        generos: ["Romance", "Drama"],
-        duracaoMinutos: 128,    
-        temporadas: 0
-    },
-    { 
-        id: 12,
-        titulo: "O Último Feitiço",
-        tipo: "Filme",
-        generos: ["Fantasia", "Aventura"],
-        duracaoMinutos: 122,    
-        temporadas: 0
-    },
-    { 
-        id: 13,
-        titulo: "Café das Seis",
-        tipo: "Série",
-        generos: ["Romance", "Comédia"],
-        duracaoMinutos: 134,    
-        temporadas: 7
-    },
-    { 
-        id: 14,
-        titulo: "Ritmo das Ruas",
-        tipo: "Série",
-        generos: ["Musical", "Comédia"],
-        duracaoMinutos: 134,    
-        temporadas: 7
-    }
-];
-
-console.log(catalogo)
+// RF02 - CATALOGO DE CONTEUDOS 
+    // CLASSES
+        // Pai
+        class Conteudo {
+            constructor(id, titulo, tipo, generos, duracaoMinutos) {
+                this.id = id;
+                this.titulo = titulo;
+                this.tipo = tipo;
+                this.generos = generos;
+                this.duracaoMinutos = duracaoMinutos;
+            }
+            exibirDados(){
+                console.log(`Título: ${this.titulo} - Tipo: ${this.tipo} - Gênero(s): ${this.generos} - Duração em Minutos: ${this.duracaoMinutos}`);
+            }
+        };
+        // Filha(s)
+        // Filme
+        class Filme extends Conteudo {
+            constructor(id,titulo,tipo,generos,duracaoMinutos) {
+            super(id,titulo,"Filme",generos,duracaoMinutos);
+            }
+        };
+        // Série
+        class Serie extends Conteudo {
+            constructor(id,titulo,tipo,generos,duracaoMinutos,temporadas) {
+            super(id,titulo,"Série",generos,duracaoMinutos);
+            this.temporadas = temporadas;
+            }
+            exibirDadosSerie(){
+            this.exibirDados();
+                console.log(` - Temporada(s): ${this.temporadas}`);
+            }
+        };
+    // ARRAY CATALOGO DE CONTEUDOS
+        const catalogo = [ 
+            new Filme( 1, "Horizonte Sem Fim", this.tipo, ["Ficção Científica", "Drama", "Aventura"], 148 ), 
+            new Serie( 2, "Cidade das Sombras", this.tipo, ["Suspense", "Crime", "Drama"], 75, 6 ), 
+            new Filme( 3, "Operação Tempestade", this.tipo, ["Ação", "Guerra"], 129 ), 
+            new Serie( 4, "Código Eclipse", this.tipo, ["Ficção Científica", "Mistério"], 56, 4 ), 
+            new Filme( 5, "Missão Final", this.tipo, ["Ação", "Aventura"], 120 ), 
+            new Serie( 6, "Ecos do Amanhã", this.tipo, ["Drama", "Ficção Científica", "Mistério"], 215, 15 ), 
+            new Filme( 7, "O Reino Encantado", this.tipo, ["Animação", "Família", "Fantasia"], 118 ), 
+            new Serie( 8, "Confusão no Prédio", this.tipo, ["Comédia"], 145, 9 ), 
+            new Filme( 10, "A Mansão da Neblina", this.tipo, ["Terror", "Suspense"], 145 ), 
+            new Filme( 11, "Corações ao Entardecer", this.tipo, ["Romance", "Drama"], 128 ), 
+            new Filme( 12, "O Último Feitiço", this.tipo, ["Fantasia", "Aventura"], 122 ), 
+            new Serie( 13, "Café das Seis", this.tipo, ["Romance", "Comédia"], 134, 7 ), 
+            new Serie( 14, "Ritmo das Ruas", this.tipo, ["Musical", "Comédia"], 134, 7 ) 
+        ];
+        //console.log(catalogo);
 
 // RF03 - CALCULAR COMPATIBILIDADE CONTEUDO
 
